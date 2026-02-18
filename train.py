6fcf2337d3c3c3331c65f671e74fd451571a8d80
Zichen Luo <zichen.luo@gmail.com>
2026-02-19 04:21:46 +0800

    fix 193 194

--- train.py
+++ train.py
@@ -211,7 +211,7 @@ def get_config(model_name):
             elif model_name == 'neon191':
                 config['d_model'], config['d_ff'] = 272, 1111 # 5.28M Lightweight Bottleneck Gate
             elif model_name in ['neon193', 'neon194']:
-                config['d_model'], config['d_ff'] = 272, 1066 # 5.28M Residual Flip / Polarity Gate
+                config['d_model'], config['d_ff'] = 272, 981 # 5.0M Residual Flip / Polarity Gate
             elif model_name in ['neon195', 'neon196']:
                 config['d_model'], config['d_ff'] = 272, 1072 # 5.0M Bipolar Intent variants
             else: config['d_ff'] = 512
